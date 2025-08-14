c97ccc867e32e5517de11dd15664e2d7e030e75b
Tech-with-anmol <anmollklfh@gmail.com>
2025-08-14 15:53:48 +0000

    h

--- openmf.cpp
+++ openmf.cpp
@@ -17,7 +17,7 @@ const char *fragmentShaderSource =
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    " FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+    " FragColor = vec4(0.2f, 0.3f, 0.8f, 0.23f);\n"
     "}\n";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -60,12 +60,14 @@ int main() {
   unsigned int shaderProgram;
   unsigned int VBO;
   unsigned int VAO;
+  unsigned int EBO;
   int width, height;
 
   glGenBuffers(1, &VBO);
   glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
 
-  glBindVertexArray(VAO);
+  
 
   shaderProgram = glCreateProgram();
 
@@ -107,16 +109,31 @@ int main() {
   glDeleteShader(fragmentShader);
 
   float vertices[] = {
-    -1.0f, -1.0f, 0.0f, // Bottom-left
-     0.0f, -0.0007f, 0.0f, // Bottom-right
-     1.0f,  1.0f, 0.0f  // Top
+    -0.9f, -0.9f, 0.0f, // Bottom-left
+     0.9f, -0.9f, 0.0f, // Bottom-right
+     0.9f,  0.9f, 0.0f, // Top-right
+    -0.9f,  0.9f, 0.0f  // Top-left
   };
+
+  unsigned int indices[] = {
+    0, 1, 2,
+    0, 2, 3,
+  };
+
+  // Bind VAO
+
+  glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -131,7 +148,8 @@ int main() {
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
